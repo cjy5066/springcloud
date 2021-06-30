da2327b477f26371f43f54bb32ce71c8eaf991d6
cjy5066 <cjy_5066@aliyun.com>
2021-06-30 15:32:20 +0800

    热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/sentinel/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/sentinel/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -44,5 +47,24 @@ public class FlowLimitController {
         int i=1/0;
         return "------testF";
     }
+
+
+    /*
+          测试热点规则
+     */
+    @GetMapping("/testHotKey")
+    // value的值是在sentinel 热点规则中的配置资源名  @RequestParam中的 required 可写可不写   blockHandler表示兜底方法的名称
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    //兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
+    }
+
 
 }
